2ad34152a9d731346db6495c6d7bd90a73d0fa05
Cheng Li <cheng@Chengs-MacBook-Pro.local>
2017-06-05 21:59:11 -0700

    sensorConfigureController work

---
+++ src/controller/SensorConfigureController.java
@@ -0,0 +1,109 @@
+package controller;
+
+import model.*;
+
+import javax.swing.*;
+import javax.swing.border.TitledBorder;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+
+
+
+public class SensorConfigureController extends JPanel
+{
+	private SensorBank sensorBank;
+	private SensorSchedule sensorSchedule;
+
+	private JComboBox groupCombo;
+	private JLabel groupLabel,startTimeLabel,endTimeLabel;
+	private JTextField sTTextField, eTTextField;
+
+	public SensorConfigureController(SensorBank controlledSensorBank, SensorSchedule controlledSensorSchedule)
+	{
+		super();
+		sensorBank  = controlledSensorBank;
+		sensorSchedule = controlledSensorSchedule;
+
+		groupLabel = new JLabel("Sensor Group ");
+		startTimeLabel = new JLabel("Start Time ");
+		endTimeLabel = new JLabel("End Time ");
+
+		sTTextField = new JTextField(10);
+		eTTextField = new JTextField(10);
+
+
+
+
+		JComboBox cbx = groupComboBox();
+
+		JButton scheduleButton = new JButton("Schedule ");
+
+		setLayout(new FlowLayout());
+		add(groupLabel);
+		add(cbx);
+		add(startTimeLabel);
+		add(sTTextField);
+		add(endTimeLabel);
+		add(eTTextField);
+		add(scheduleButton);
+
+
+		scheduleButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent event)
+			{
+				try
+				{
+					SensorTime startTime = new SensorTime(sTTextField.getText());
+					SensorTime endTime =new SensorTime(eTTextField.getText());
+					sensorSchedule.insertTimeRange(SensorGroup.valueOf(groupLabel.getText()),startTime,endTime);
+				}
+				catch(TimeFormatException exception)
+				{
+					JOptionPane.showMessageDialog(SensorConfigureController.this,"Please enter a valid time","Error",JOptionPane.ERROR_MESSAGE);
+
+				}
+			}
+
+		});
+	}
+	public JComboBox groupComboBox()
+	{
+		String[] group = {" ","NORTH","SOUTH","EAST","WEST"};
+		groupCombo = new JComboBox(group);
+
+		groupCombo.setForeground(Color.BLUE);
+		groupCombo.setFont(new Font("Arial", Font.BOLD, 20));
+      	groupCombo.setEditable(true);
+
+      	groupCombo.addActionListener(new ActionListener()
+      	{
+      		public void actionPerformed(ActionEvent e)
+      		{
+      			JComboBox cb= (JComboBox)e.getSource();
+      			String type = (String)cb.getSelectedItem();
+      			groupLabel.setText(type);
+	
+      		}
+      	});
+      	return groupCombo;
+	}
+	public static void main(String args[])	
+	{
+		SensorBank sb = new SensorBank();
+		SensorSchedule  ss = new SensorSchedule();
+
+		SensorConfigureController scc = new SensorConfigureController(sb,ss);
+		JFrame f = new JFrame();
+		f.getContentPane().add(scc);
+		f.pack();
+		f.setVisible(true);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	}
+
+
+}
+
+
--- src/model/SensorSchedule.java
+++ src/model/SensorSchedule.java
@@ -30,8 +30,13 @@ public class SensorSchedule
 	}
 	public void insertTimeRange(SensorGroup group, SensorTime startTime, SensorTime endTime)
 	{
+		System.out.println(groupScheduleMap.get(SensorGroup.NORTH));
 		ArrayList<TimeRange> timeArray = groupScheduleMap.get(group);
 		timeArray.add(new TimeRange(startTime, endTime));
+		System.out.println(groupScheduleMap.get(SensorGroup.NORTH));
+
+
+
 
 	}
 
